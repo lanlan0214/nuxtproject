80ef3720e54ed9f2b799ae91c8b5fd72cb05a435
Frank141 <https://github.com/lanlan0214/TodayDo.git>
2023-06-08 00:02:18 +0800

    202306081202

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Home Page</div>
+  <div class="flex flex-ol py-10">
+    <div>
+      <h2 class="text-2xl font-bold text-center">Nuxt Movie App</h2>
+    </div>
+    <div class="flex justify-center items-center h-32">
+      <input class="px-2 border border-gray-800 rounded-md min-w-64" type="text" v-model="searchTerm"
+        placeholder="Search...">
+    </div>
+    {{ searchTerm }}
+  </div>
 </template>
+
+<script setup lang="ts">
+// 這裡使用API響應
+import { APIResponse } from '~/types/APIResponse';
+
+const searchTerm = ref('');
+
+const url = computed(() => {
+  return `api/movies/search`
+})
+
+</script>
